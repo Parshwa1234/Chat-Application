8341d1c7a041888bb8fbbf9c164accc7d74d0f06
Parshwa <parshwa22nd@gmail.com>
2025-08-09 20:27:23 +0530

    changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "https://real-time-chatty-application.onrender.com/api";
+const API_URL = "https://real-time-chatty-application.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: API_URL,
